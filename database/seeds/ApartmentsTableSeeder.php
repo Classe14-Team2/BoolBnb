4ec6264a56d8ed6667c58bda59ffdabccd3ff87a
Corrado <corradoct@hotmail.it>
2020-09-23 12:31:12 +0200

    Created Apartments seeder

--- database/seeds/ApartmentsTableSeeder.php
+++ database/seeds/ApartmentsTableSeeder.php
@@ -13,6 +13,23 @@ class ApartmentsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        
+        for ($i=0; $i < 3; $i++) {
+          $newApartment = new Apartment();
+          $newApartment->user_id = 1;
+          $newApartment->title = $faker->text(100);
+          $newApartment->description = $faker->text(200);
+          $newApartment->rooms = $faker->numberBetween(1, 5);
+          $newApartment->beds = $faker->numberBetween(1, 5);
+          $newApartment->baths = $faker->numberBetween(1, 3);
+          $newApartment->square_meters = $faker->numberBetween(60, 250);
+          $newApartment->address = $faker->streetAddress;
+          $newApartment->city = $faker->city;
+          $newApartment->country = $faker->country;
+          $newApartment->price = $faker->randomFloat($nbMaxDecimals = 2, $min = 20, $max = 999);
+          $newApartment->image = $faker->imageUrl();
+          $newApartment->lat = $faker->latitude($min = -90, $max = 90);
+          $newApartment->lon = $faker->longitude($min = -180, $max = 180);
+          $newApartment->save();
+        }
     }
 }
